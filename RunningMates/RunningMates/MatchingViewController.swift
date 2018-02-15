e076f4c4f085ce12b2893678390665a6a2a8b3b8
watermandrew <imac@MacBook-Pro-5.local>
2018-02-15 14:51:05 -0500

    Divya and I have decided to do away with the Matching.xib file and are directly editing a view using the main storyboard. App stopped crashing now.

---
+++ RunningMates/RunningMates/MatchingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Divya Kalidindi on 2/15/18.
+//  Copyright © 2018 Divya Kalidindi. All rights reserved.
+//
+
+import UIKit
+
+class MatchingViewController: UIViewController {
+   
+   // MARK: Properties
+   
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+       super.didReceiveMemoryWarning()
+       // Dispose of any resources that can be recreated.
+   }
+   
+   //MARK: Actions
+
+    @IBAction func matchButton(_ sender: UIButton) {
+        
+        nameLabel.text = "Divya Kalidindi"
+    }
+    //        addSubview(matchingView)
+   
+}
