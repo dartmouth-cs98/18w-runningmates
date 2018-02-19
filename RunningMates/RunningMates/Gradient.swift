a0ae0797e63f34e1df21b6d25e7a06980dded344
dali <dali@dalis-MacBook-Air.local>
2018-02-19 17:24:50 -0500

    changed login ui, still more to do

---
+++ RunningMates/RunningMates/Gradient.swift
@@ -0,0 +1,20 @@
+// Found this code for creating a gradient at https://stackoverflow.com/questions/8164609/is-there-a-way-to-make-gradient-background-color-in-the-interface-builder
+
+import UIKit
+
+@IBDesignable
+final class GradientView: UIView {
+    @IBInspectable var startColor: UIColor = UIColor.clear
+    @IBInspectable var endColor: UIColor = UIColor.clear
+    
+    override func draw(_ rect: CGRect) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = CGRect(x: CGFloat(0),
+                                y: CGFloat(0),
+                                width: superview!.frame.size.width,
+                                height: superview!.frame.size.height)
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
+        gradient.zPosition = -1
+        layer.addSublayer(gradient)
+    }
+}
