f05ae563d74b09910370f0e17611c786d7df559b
stopic13 <sara.topic.18@dartmouth.edu>
2018-02-24 20:57:01 -0500

    TRULY AGAINST ALL ODDS I HAVE FIGURED OUT HOW TO CHANGE VIEWS

--- RunningMates/RunningMates/ViewController.swift
+++ RunningMates/RunningMates/ViewController.swift
@@ -10,7 +10,7 @@ import Alamofire
 import UIKit
 import WebKit
 
-    class ViewController: UIViewController, WKUIDelegate {
+    class ViewController: UIViewController, WKUIDelegate, UINavigationControllerDelegate {
 
         var webView: WKWebView!
         var rootURl: String = "https://running-mates.herokuapp.com/"
@@ -49,10 +49,15 @@ import WebKit
             scope: "write", state:"mystate",
             success: { credential, response, parameters in
                 print("response token: ")
-                
+                let  imagePickerController = self.storyboard?.instantiateViewController(withIdentifier: "matching") as! MatchingViewController
+                self.present(imagePickerController, animated: true, completion: nil)
+
                 print(credential.oauthToken)
                 let params: Parameters = [
                     "token": credential.oauthToken,
+                    
+                    
+
                 ]
                 let Url = self.rootURl + "api/stravaSignup"
                 
