75445df5fa86e9fe289249d99627b27d127256a8
stopic13 <sara.topic.18@dartmouth.edu>
2018-03-04 15:18:55 -0500

    added double range slider to filter page

--- RunningMates/RunningMates/FilterViewController.swift
+++ RunningMates/RunningMates/FilterViewController.swift
@@ -11,8 +11,12 @@ import DLRadioButton
 
 class FilterViewController: UIViewController {
    
-    let rangeSlider = RangeSlider(frame: CGRect.zero)
+    let ageSlide = RangeSlider(frame: CGRect.zero)
+    let paceSlide = RangeSlider(frame: CGRect.zero)
+    let proxSlide = RangeSlider(frame: CGRect.zero)
 
+    
+   // @IBOutlet weak var fuckUXcode: RangeSlider!
     
     @IBOutlet weak var femaleButton: DLRadioButton!
     var femaleButtonSwitch = 1
@@ -27,17 +31,26 @@ class FilterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        rangeSlider.backgroundColor = UIColor.red
-        view.addSubview(rangeSlider)
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        view.addSubview(ageSlide)
+        view.addSubview(paceSlide)
+        view.addSubview(proxSlide)
+
+        ageSlide.addTarget(self, action: Selector(("ageSliderValueChanged:")), for: .valueChanged)
+        paceSlide.addTarget(self, action: Selector(("paceSliderValueChanged:")), for: .valueChanged)
+        proxSlide.addTarget(self, action: Selector(("paceSliderValueChanged:")), for: .valueChanged)
+
+            }
     
     
     override func viewDidLayoutSubviews() {
         let margin: CGFloat = 20.0
         let width = view.bounds.width - 2.0 * margin
-        rangeSlider.frame = CGRect(x: margin, y: margin + topLayoutGuide.length,
+        ageSlide.frame = CGRect(x: margin, y: 200.0 + topLayoutGuide.length,
                                    width: width, height: 31.0)
+        paceSlide.frame = CGRect(x: margin, y: 375.0 + topLayoutGuide.length,
+                                width: width, height: 31.0)
+        proxSlide.frame = CGRect(x: margin, y: 500.0 + topLayoutGuide.length,
+                                 width: width, height: 31.0)
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,6 +59,14 @@ class FilterViewController: UIViewController {
     }
     
     //MARK: Actions
+//    func rangeSliderValueChanged(rangeSlider: RangeSlider) {
+//        print("Range slider value changed: (\(rangeSlider.lowerValue) \(rangeSlider.upperValue))")
+//    }
+//
+    @IBAction func rangeSliderValueChanged(_ sender: RangeSlider) {
+        print("Range slider value changed: (\(ageSlide.lowerValue) \(ageSlide.upperValue))")
+
+    }
     
     @IBAction func maleButtonSelected(_ sender: Any) {
         print("selected male")
--- RunningMates/RunningMates/RangeSlider.swift
+++ RunningMates/RunningMates/RangeSlider.swift
@@ -1,66 +1,160 @@
-//
-//  RangeSlider.swift
-//  RunningMates
-//
-//  Created by Sara Topic on 01/03/2018.
-//  Copyright © 2018 Apple Inc. All rights reserved.
-//
-//  source: https://www.raywenderlich.com/76433/how-to-make-a-custom-control-swift
 import UIKit
 import QuartzCore
 
 class RangeSlider: UIControl {
-
-        var minimumValue = 0.0
-        var maximumValue = 1.0
-        var lowerValue = 0.2
-        var upperValue = 0.8
+    let trackLayer = RangeSliderTrackLayer()
+    let lowerThumbLayer = RangeSliderThumbLayer()
+    let upperThumbLayer = RangeSliderThumbLayer()
+    var previousLocation = CGPoint()
     
-    let trackLayer = CALayer()
-    let lowerThumbLayer = CALayer()
-    let upperThumbLayer = CALayer()
+    var minimumValue: Double = 18 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var maximumValue: Double = 99 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var lowerValue: Double = 18 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var upperValue: Double = 99 {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    var trackTintColor: UIColor = UIColor(white: 0.9, alpha: 1.0) {
+        didSet {
+            trackLayer.setNeedsDisplay()
+        }
+    }
+    
+    var trackHighlightTintColor: UIColor = UIColor(red: 1.0, green: 0.65, blue: 0.35, alpha: 1.0) {
+        didSet {
+            trackLayer.setNeedsDisplay()
+        }
+    }
+    
+    var thumbTintColor: UIColor = UIColor(red: 1.0, green: 0.65, blue: 0.35, alpha: 1.0){
+        didSet {
+            lowerThumbLayer.setNeedsDisplay()
+            upperThumbLayer.setNeedsDisplay()
+        }
+    }
+    
+    var curvaceousness: CGFloat = 1.0 {
+        didSet {
+            trackLayer.setNeedsDisplay()
+            lowerThumbLayer.setNeedsDisplay()
+            upperThumbLayer.setNeedsDisplay()
+        }
+    }
     
     var thumbWidth: CGFloat {
         return CGFloat(bounds.height)
     }
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        trackLayer.backgroundColor = UIColor.blueColor().CGColor
-//        layer.addSublayer(trackLayer)
-//
-//        lowerThumbLayer.backgroundColor = UIColor.greenColor().CGColor
-//        layer.addSublayer(lowerThumbLayer)
-//
-//        upperThumbLayer.backgroundColor = UIColor.greenColor().CGColor
-//        layer.addSublayer(upperThumbLayer)
-//
-//        updateLayerFrames()
-//    }
-//
-//    required init(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
-//
-//    func updateLayerFrames() {
-//        trackLayer.frame = bounds.rectByInsetting(dx: 0.0, dy: bounds.height / 3)
-//        trackLayer.setNeedsDisplay()
-//
-//        let lowerThumbCenter = CGFloat(positionForValue(lowerValue))
-//
-//        lowerThumbLayer.frame = CGRect(x: lowerThumbCenter - thumbWidth / 2.0, y: 0.0,
-//                                       width: thumbWidth, height: thumbWidth)
-//        lowerThumbLayer.setNeedsDisplay()
-//
-//        let upperThumbCenter = CGFloat(positionForValue(upperValue))
-//        upperThumbLayer.frame = CGRect(x: upperThumbCenter - thumbWidth / 2.0, y: 0.0,
-//                                       width: thumbWidth, height: thumbWidth)
-//        upperThumbLayer.setNeedsDisplay()
-//    }
-//
-//    func positionForValue(value: Double) -> Double {
-//        return Double(bounds.width - thumbWidth) * (value - minimumValue) /
-//            (maximumValue - minimumValue) + Double(thumbWidth / 2.0)
-//    }
+    override var frame: CGRect {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        trackLayer.rangeSlider = self
+        trackLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(trackLayer)
+        
+        lowerThumbLayer.rangeSlider = self
+        lowerThumbLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(lowerThumbLayer)
+        
+        upperThumbLayer.rangeSlider = self
+        upperThumbLayer.contentsScale = UIScreen.main.scale
+        layer.addSublayer(upperThumbLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func updateLayerFrames() {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
+        trackLayer.setNeedsDisplay()
+        
+        let lowerThumbCenter = CGFloat(positionForValue(value: lowerValue))
+        
+        lowerThumbLayer.frame = CGRect(x: lowerThumbCenter - thumbWidth / 2.0, y: 0.0, width: thumbWidth, height: thumbWidth)
+        lowerThumbLayer.setNeedsDisplay()
+        
+        let upperThumbCenter = CGFloat(positionForValue(value: upperValue))
+        upperThumbLayer.frame = CGRect(x: upperThumbCenter - thumbWidth / 2.0, y: 0.0,
+                                       width: thumbWidth, height: thumbWidth)
+        upperThumbLayer.setNeedsDisplay()
+        
+        CATransaction.commit()
+    }
+    
+    func positionForValue(value: Double) -> Double {
+        _ = Double(thumbWidth)
+        return Double(bounds.width - thumbWidth) * (value - minimumValue) /
+            (maximumValue - minimumValue) + Double(thumbWidth / 2.0)
+    }
+    
+    // Touch handlers
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        previousLocation = touch.location(in: self)
+        
+        // Hit test the thumb layers
+        if lowerThumbLayer.frame.contains(previousLocation) {
+            lowerThumbLayer.highlighted = true
+        } else if upperThumbLayer.frame.contains(previousLocation) {
+            upperThumbLayer.highlighted = true
+        }
+        
+        return lowerThumbLayer.highlighted || upperThumbLayer.highlighted
+    }
+    
+    func boundValue(value: Double, toLowerValue lowerValue: Double, upperValue: Double) -> Double {
+        return min(max(value, lowerValue), upperValue)
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let location = touch.location(in: self)
+        
+        // 1. Determine by how much the user has dragged
+        let deltaLocation = Double(location.x - previousLocation.x)
+        let deltaValue = (maximumValue - minimumValue) * deltaLocation / Double(bounds.width - bounds.height)
+        
+        previousLocation = location
+        
+        // 2. Update the values
+        if lowerThumbLayer.highlighted {
+            lowerValue += deltaValue
+            lowerValue = boundValue(value: lowerValue, toLowerValue: minimumValue, upperValue: upperValue)
+        } else if upperThumbLayer.highlighted {
+            upperValue += deltaValue
+            upperValue = boundValue(value: upperValue, toLowerValue: lowerValue, upperValue: maximumValue)
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        lowerThumbLayer.highlighted = false
+        upperThumbLayer.highlighted = false
+    }
 }
