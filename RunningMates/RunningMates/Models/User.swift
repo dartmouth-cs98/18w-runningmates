50eaff6ba460d6cffb8a7a441c954466a5dc25c3
dali <shea.e.wojciehowski.17@dartmouth.edu>
2018-04-05 17:18:24 -0400

    updated user model

--- RunningMates/RunningMates/Models/User.swift
+++ RunningMates/RunningMates/Models/User.swift
@@ -23,14 +23,15 @@ enum UserInitError: Error {
 }
 
 class User: NSObject, NSCoding {
-    var id: String?
     var firstName: String?
     var lastName: String
     var imageURL: String
+    var images: [String:Any]?
     var bio: String
     var gender: String
     var age: Int
     var location: [Float]
+    var desiredGoals: [Any]?
     var swipes: [String: Int]?
     var mates: [Any]?
     var potentialMates: [Any]?
@@ -40,37 +41,13 @@ class User: NSObject, NSCoding {
     var password: String?
     var token: String?
     var preferences: [String:Any]?
+    var thirdPartyIds: [String:Any]?
     var data: [String:Any]?
     
-
-//    //MARK: Initialization Dont believe we need to initialize twice with the lowere json 
-//    init(id: String, firstName: String, lastName: String, imageURL: String, bio: String, gender: String, age: Int, location: [Float], swipes: [String: Int], mates: [Any], potentialMates: [Any], blockedMates: [Any], seenProfiles: [Any], email: String, password: String, token: String, preferences: [String:Any], data: [String:Any]) {
-//        self.id = id
-//        self.firstName = firstName
-//        self.lastName = lastName
-//        self.imageURL = imageURL
-//        self.bio = bio
-//        self.gender = gender
-//        self.age = age
-//        self.location = location
-//        self.swipes = swipes
-//        self.mates = mates
-//        self.potentialMates = potentialMates
-//        self.seenProfiles = seenProfiles
-//        self.blockedMates = blockedMates
-//        self.email = email
-//        self.password = password
-//        self.token = token
-//        self.preferences = preferences
-//        self.data = data
-//    }
     
     //JSON initializer
     // https://developer.apple.com/swift/blog/?id=37
     init?(json: [String: Any]) throws {
-        guard let id = json["id"] as! String? else {
-            throw UserInitError.invalidId
-        }
         guard let firstName = json["firstName"] as! String? else {
             throw UserInitError.invalidFirstName
         }
@@ -92,11 +69,14 @@ class User: NSObject, NSCoding {
         guard let location = json["location"] as! [Float]? else {
             throw UserInitError.invalidLocation
         }
+        let images = json["images"] as! [String: Any]?
         let swipes = json["swipes"] as! [String: Int]?
         let mates = json["mates"] as! [Any]?
+        let desiredGoals = json["desiredGoals"] as! [Any]?
         let potentialMates = json["potentialMates"] as! [Any]?
         let seenProfiles = json["seenProfiles"] as! [Any]?
         let blockedMates = json["blockedMates"] as! [Any]?
+        let thirdPartyIds = json["thirdPartyIds"] as! [String: Any]?
         guard let email = json["email"] as! String? else {
             throw UserInitError.invalidEmail
         }
@@ -105,14 +85,15 @@ class User: NSObject, NSCoding {
         let preferences = json["preferences"] as! [String:Any]?
         let data = json["data"] as! [String:Any]?
 
-        self.id = id
         self.firstName = firstName
         self.lastName = lastName
         self.imageURL = imageURL
+        self.images = images
         self.bio = bio
         self.gender = gender
         self.age = age
         self.location = location
+        self.desiredGoals = desiredGoals
         self.swipes = swipes
         self.mates = mates
         self.potentialMates = potentialMates
@@ -122,18 +103,20 @@ class User: NSObject, NSCoding {
         self.password = password
         self.token = token
         self.preferences = preferences
+        self.thirdPartyIds = thirdPartyIds
         self.data = data
     }
     
     struct PropertyKey {
-        static let id = "id"
         static let firstName = "firstName"
         static let lastName = "lastName"
         static let imageURL = "imageURL"
+        static let images = "images"
         static let bio = "bio"
         static let gender = "gender"
         static let age = "age"
         static let location = "location"
+        static let desiredGoals = "desiredGoals"
         static let swipes = "swipes"
         static let mates = "mates"
         static let potentialMates = "potentialMates"
@@ -143,19 +126,21 @@ class User: NSObject, NSCoding {
         static let password = "password"
         static let token = "token"
         static let preferences = "preferences"
+        static let thirdPartyIds = "thirdPartyIds"
         static let data = "data"
     }
     
     func encode(with aCoder: NSCoder) {
-        aCoder.encode(id, forKey: PropertyKey.id)
         aCoder.encode(firstName, forKey: PropertyKey.firstName)
         aCoder.encode(lastName, forKey: PropertyKey.lastName)
         aCoder.encode(imageURL, forKey: PropertyKey.imageURL)
+        aCoder.encode(images, forKey: PropertyKey.images)
         aCoder.encode(bio, forKey: PropertyKey.bio)
         aCoder.encode(gender, forKey: PropertyKey.gender)
         aCoder.encode(age, forKey: PropertyKey.age)
         aCoder.encode(location, forKey: PropertyKey.location)
         aCoder.encode(swipes, forKey: PropertyKey.swipes)
+        aCoder.encode(desiredGoals, forKey: PropertyKey.desiredGoals)
         aCoder.encode(mates, forKey: PropertyKey.mates)
         aCoder.encode(potentialMates, forKey: PropertyKey.potentialMates)
         aCoder.encode(seenProfiles, forKey: PropertyKey.seenProfiles)
@@ -164,23 +149,23 @@ class User: NSObject, NSCoding {
         aCoder.encode(password, forKey: PropertyKey.password)
         aCoder.encode(token, forKey: PropertyKey.token)
         aCoder.encode(preferences, forKey: PropertyKey.preferences)
+        aCoder.encode(thirdPartyIds, forKey: PropertyKey.thirdPartyIds)
         aCoder.encode(data, forKey: PropertyKey.data)
     }
     
     required convenience init?(coder aDecoder: NSCoder) {
         
-        // The name is required. If we cannot decode a name string, the initializer should fail.
-        //guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+        // The first name is required. If we cannot decode a name string, the initializer should fail.
+        guard let firstName = aDecoder.decodeObject(forKey: PropertyKey.firstName) as? String else {
             os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
             return nil
-        
+        }
         // Because photo is an optional property of Meal, just use conditional cast.
        // let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
         
         //let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
         
-        // Must call designated initializer.
-        //self.init(name: name, photo: photo, rating: rating)
+        self.init(name: name, photo: photo, rating: rating)
         
     }
 }
