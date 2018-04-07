8ccb7c2fa8bf6b524371a7db4ccbee21fc7eb0af
stopic13 <sara.topic.18@dartmouth.edu>
2018-04-07 16:15:16 -0400

    creating settings page

--- RunningMates/RunningMates/Controllers/SettingsViewController.swift
+++ RunningMates/RunningMates/Controllers/SettingsViewController.swift
@@ -12,12 +12,46 @@ class SettingsViewController: UIViewController {
     
     // MARK: Properties
     
-    //@IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var privacyButton: UIButton!
+
+    @IBOutlet weak var notificationButton: UIButton!
+    @IBOutlet weak var termsButton: UIButton!
+    @IBOutlet weak var communityButton: UIButton!
+    @IBOutlet weak var safetyButton: UIButton!
+    @IBOutlet weak var creditButton: UIButton!
     
+    @IBOutlet weak var signOutButton: UIButton!
     //  @IBOutlet weak var genderLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // learned to add border stuff here: https://stackoverflow.com/questions/37230084/how-to-change-uibutton-border-color-black-to-white
+        
+        self.privacyButton.layer.borderWidth = 3.0;
+        self.privacyButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.notificationButton.layer.borderWidth = 3.0;
+        self.notificationButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.termsButton.layer.borderWidth = 3.0;
+        self.termsButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.communityButton.layer.borderWidth = 3.0;
+        self.communityButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.safetyButton.layer.borderWidth = 3.0;
+        self.safetyButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.creditButton.layer.borderWidth = 3.0;
+        self.creditButton.layer.borderColor = UIColor.darkGray.cgColor;
+        
+        self.signOutButton.layer.borderWidth = 3.0;
+        self.signOutButton.layer.borderColor = UIColor.darkGray.cgColor;
+        self.signOutButton.layer.cornerRadius = 5
+
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
