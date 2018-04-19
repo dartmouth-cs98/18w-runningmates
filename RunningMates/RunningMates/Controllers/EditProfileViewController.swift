199a781d4690e3cf994fead405fd43180cb4ba18
dali <shea.e.wojciehowski.17@dartmouth.edu>
2018-04-19 17:23:40 -0400

    edits

--- RunningMates/RunningMates/Controllers/EditProfileViewController.swift
+++ RunningMates/RunningMates/Controllers/EditProfileViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2018 Apple Inc. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import OAuthSwift
+import Alamofire
+import WebKit
+
+class EditProfileViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerOptions.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->
+        String? {
+            return pickerOptions[row]
+    }
+    
+
+    override func viewDidLoad() {
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+}
