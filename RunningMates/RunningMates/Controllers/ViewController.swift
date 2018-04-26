ff27bec4447e4d4fdf59e4e4b3b0ccea3d295eb8
dali <shea.e.wojciehowski.17@dartmouth.edu>
2018-04-26 17:04:53 -0400

    fixed error where prev token was not defined

--- RunningMates/RunningMates/Controllers/ViewController.swift
+++ RunningMates/RunningMates/Controllers/ViewController.swift
@@ -104,27 +104,29 @@ extension UIViewController {
                         let token = (jsonUser["token"] as? String)
                         let prevToken = String(describing: UserDefaults.standard.value(forKey: "token")!)
                         // Check to see if this user is already saved in the UserDefaults, if so, we don't need to save all of their information again.
-                        if !(token == prevToken) {
-                            UserDefaults.standard.set(user["firstName"], forKey: "firstName")
-                            UserDefaults.standard.set(user["email"], forKey: "email")
-                            UserDefaults.standard.set(token, forKey: "token")
-                            if (user["lastName"] != nil) {
-                                UserDefaults.standard.set(user["lastName"], forKey: "lastName")
-                            }
-                            
-                            if (user["imageURL"] != nil) {
-                                UserDefaults.standard.set(user["imageURL"], forKey: "imageURL")
-                            }
-                            
-                            if (user["images"] != nil) {
-                                UserDefaults.standard.set(user["images"], forKey: "images")
-                            }
-                            
-                            if (user["data"] != nil) {
-                                UserDefaults.standard.set(user["data"], forKey: "data")
-                            }
-                            if (user["desiredGoals"] != nil) {
-                                UserDefaults.standard.set(user["desiredGoals"], forKey: "desiredGoals")
+                        if (prevToken != nil) {
+                            if !(token == prevToken) {
+                                UserDefaults.standard.set(user["firstName"], forKey: "firstName")
+                                UserDefaults.standard.set(user["email"], forKey: "email")
+                                UserDefaults.standard.set(token, forKey: "token")
+                                if (user["lastName"] != nil) {
+                                    UserDefaults.standard.set(user["lastName"], forKey: "lastName")
+                                }
+                                
+                                if (user["imageURL"] != nil) {
+                                    UserDefaults.standard.set(user["imageURL"], forKey: "imageURL")
+                                }
+                                
+                                if (user["images"] != nil) {
+                                    UserDefaults.standard.set(user["images"], forKey: "images")
+                                }
+                                
+                                if (user["data"] != nil) {
+                                    UserDefaults.standard.set(user["data"], forKey: "data")
+                                }
+                                if (user["desiredGoals"] != nil) {
+                                    UserDefaults.standard.set(user["desiredGoals"], forKey: "desiredGoals")
+                                }
                             }
                         }
                     }
