16cc3ab6429a570eb8df8f5848f5010efc61d55d
stopic13 <sara.topic.18@dartmouth.edu>
2018-05-02 16:27:46 -0400

    making mapview not take up a whole screen

--- RunningMates/RunningMates/SafeTrackViewController.swift
+++ RunningMates/RunningMates/SafeTrackViewController.swift
@@ -13,13 +13,23 @@ class SafeTrackViewController: UIViewController {
     
     // You don't need to modify the default init(nibName:bundle:) method.
     
+    @IBOutlet weak var mapViewArea: UIView!
+    
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
+        super.loadView()
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
+        let rect = CGRect(
+            origin: CGPoint(x: 0, y: 0),
+            size: CGSize(width: 245, height: 245)
+        )
+        let mapView = GMSMapView.map(withFrame: rect, camera: camera)
+        //var mapView = GMSMapView.mapWithFrame(CGRectMake(0, 0, 100, 100), camera: camera)
+        mapView.isMyLocationEnabled = true
+        self.mapViewArea = mapView
+        //mapViewArea = mapView
+       // view.addSubview(mapView)
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
