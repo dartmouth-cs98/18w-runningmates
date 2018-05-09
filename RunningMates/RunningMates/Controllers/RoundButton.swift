4017840bd57bea7f676f4f0fb18f2cbf2083c5ce
divyakalidindi <divyakalidindi@Divyas-MacBook-Air.local>
2018-05-09 18:38:26 -0400

    ui sign in page redesign

---
+++ RunningMates/RunningMates/Controllers/RoundButton.swift
@@ -0,0 +1,43 @@
+//
+//  RoundButton.swift
+//  RunningMates
+//
+//  Created by Divya Kalidindi on 5/9/18.
+//  Copyright © 2018 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+@IBDesignable class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+    
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+}
