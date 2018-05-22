76ecdf7ef0905a272f66e850c33db9f6016ab97f
divyakalidindi <divya.kalidindi.18@dartmouth.edu>
2018-05-22 17:59:20 -0400

    basic info page saves values

--- RunningMates/RunningMates/Controllers/BasicInfoViewController.swift
+++ RunningMates/RunningMates/Controllers/BasicInfoViewController.swift
@@ -17,28 +17,35 @@ class BasicInfoViewController: UIViewController {
     
     @IBOutlet weak var lastName: UITextField!
     
-    
-    
     @IBOutlet weak var dobPicker: UIDatePicker!
-    var drop: UIDropDown!
     
-    
-    
-    var genderPickerOptions: [String] = [String]()
-    var imagePicker: UIImagePickerController = UIImagePickerController()
-    
+//    var dropGender: UIDropDown!
+
+    @IBOutlet weak var dropGender: UIDropDown!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        drop = UIDropDown(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
-        drop.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
-        drop.placeholder = "Select gender..."
-        drop.options = ["Female", "Male", "Non-Binary"]
-        drop.didSelect { (option, index) in
+//        dropGender = UIDropDown(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+//        dropGender.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY)
+        dropGender.placeholder = "Select gender..."
+        dropGender.options = ["Female", "Male", "Non-Binary"]
+        dropGender.didSelect { (option, index) in
             print("You just select: \(option) at index: \(index)")
         }
-        self.view.addSubview(drop)
+        self.view.addSubview(dropGender)
+        
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        print(self.dobPicker.date)
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day,.month,.year], from: self.dobPicker.date);
+        print(components.day!)
+        print(components.month!)
+        print(components.year!)
+        
+        //send request for DOB as integers
     }
 }
 
