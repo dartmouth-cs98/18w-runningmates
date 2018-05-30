750b713418abe4333820abebabd01809cbd7b3a2
watermandrew <Drew.C.Waterman.18@Dartmouth.edu>
2018-05-30 12:55:09 -0400

    images in fullchatview are now circles

--- RunningMates/RunningMates/Controllers/FullChatViewController.swift
+++ RunningMates/RunningMates/Controllers/FullChatViewController.swift
@@ -53,19 +53,25 @@ class FullChatViewController: UIViewController, UITableViewDataSource, UITableVi
         }
         
         let url = URL(string: message["imageURL"] as! String)
-        let imgData = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+        if (url != nil) {
+            let imgData = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
 
-        let image = UIImage(data: imgData!)
-        cell.userImg.image = image
-        
-        cell.userImg.contentMode = UIViewContentMode.scaleAspectFit
-//        cell.userImg.layoutIfNeeded()
-        cell.userImg.layer.borderWidth = 1
-        cell.userImg.layer.masksToBounds = true
-        cell.userImg.layer.borderColor = UIColor.white.cgColor
-        cell.userImg.layer.cornerRadius = (cell.userImg.frame.size.width) / 2
-        cell.userImg.clipsToBounds = true
-        
+            let image = UIImage(data: imgData!)
+            cell.userImg.image = image
+            
+            cell.userImg.layer.cornerRadius = cell.userImg.frame.size.width / 2;
+            cell.userImg.clipsToBounds = true;
+            cell.userImg.layer.borderWidth = 2.5;
+            cell.userImg.layer.borderColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).cgColor
+            
+//            cell.userImg.contentMode = UIViewContentMode.scaleAspectFit
+//            cell.userImg.layoutIfNeeded()
+//            cell.userImg.layer.borderWidth = 1
+//            cell.userImg.layer.masksToBounds = true
+//            cell.userImg.layer.borderColor = UIColor.white.cgColor
+//            cell.userImg.layer.cornerRadius = (cell.userImg.frame.size.width) / 2
+//            cell.userImg.clipsToBounds = true
+        }
         cell.nameLabel?.text = displayedMembers
         cell.contentPreviewLabel?.text = content
         cell.dateLabel?.text = time
